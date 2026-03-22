73d81674bc60f913afd80d1f6cb97c04f74732ac
Emily <thewitchemily@gmail.com>
2026-03-22 01:19:22 +0100

    Added test for injecting custom code

--- worlds/pokemon_colo/PCGenerator.py
+++ worlds/pokemon_colo/PCGenerator.py
@@ -1,11 +1,12 @@
 import json, os
 from random import Random
+import struct
 
 import Utils
 
 from CommonClient import logger
 
-from gclib.gcm import GCM
+from gclib.gcm import GCM, BytesIO
 from .Helpers import StringByteFunction as sbf
 from .client.constants import CLIENT_VERSION, AP_WORLD_VERSION_NAME
 
@@ -41,20 +42,76 @@ class ColosseumRandomizer:
         logger.info("Update ISO game ID locations with AP generated seed")
         three_char_seed = local_seed[:3]
         bin_data = self.gcm.read_file_data("sys/boot.bin")
-        bin_data.seek(0x01)
+        bin_data.seek(0x00)
         bin_data.write(sbf.string_to_bytes(three_char_seed, len(three_char_seed)))
         dol_data = self.gcm.read_file_data("sys/main.dol")
-        dol_data.seek(0x399B75)
+        dol_data.seek(0x399B74)
         dol_data.write(sbf.string_to_bytes(three_char_seed, len(three_char_seed)))
-        dol_data.seek(0x39A755)
+        dol_data.seek(0x39A754)
         dol_data.write(sbf.string_to_bytes(three_char_seed, len(three_char_seed)))
+
+        # Inject custom code
+        self.inject_custom_code(dol_data)
+
         self.gcm.changed_files["sys/boot.bin"] = bin_data
         self.gcm.changed_files["sys/main.dol"] = dol_data
 
+
         # Handle rest of game randomization/AP related modifications to files
+
 
         # Saves randomized iso file, with files updated
         self.save_randomized_iso()
+
+    def inject_custom_code(self, dol_data):
+        # We inject our code into this line. It is called a few times after changing rooms or events
+        # Not quite sure what the method does, but it gets called often so we can inject custom code here
+        addr_caller = self.t1_memroy_to_dol_address(0x801904e8)
+        caller_method = [
+            0x4BE72FF9,  # bl    0x800034E0
+        ]
+        dol_data.seek(addr_caller)
+        for instr in caller_method:
+            dol_data.write(struct.pack(">I", instr))
+
+        # We inject our code into the T0 section where there are a lot of empty bytes scattered around
+        # We can use them to place our function into main.dol
+        # On load it gets loaded into the memory range of T0
+        # We can place custom code here, for example to modfiy items to be always available and not missable
+        # This is just a test for now. These bits could also be changed through the client, but would require it to be open
+        # all the time in order to play the game. So more critical stuff can be placed here directly into the iso
+        addr_code = self.t0_memroy_to_dol_address(0x800034E0)        
+        custom_method = [
+            0x7C1E212E,  # stwx  r0, r30, r4  (original replaced instruction)
+
+            # Load primary pointer
+            0x3CA08048,  # lis   r5, 0x8048
+            0x80A5ADB8,  # lwz   r5, 0xADB8(r5)    r5 = *(0x8047ADB8)
+
+            # Makes Tm46 always visible (still needs jail key)
+            # Set bit 0 (*(ptr) + 0x1C177)
+            0x3CE50002,  # addis r7, r5, 2         r7 = r5 + 0x20000
+            0x88C7C177,  # lbz   r6, 0xC177(r7)
+            0x60C60001,  # ori   r6, r6, 0x0001
+            0x98C7C177,  # stb   r6, 0xC177(r7)
+
+            # Makes Jail Key always visible (no story needed and cant miss it if other locations are visited first)
+            # Set bit 5 (*(ptr) + 0x1C119)
+            0x88C7C119,  # lbz   r6, 0xC119(r7)
+            0x60C60020,  # ori   r6, r6, 0x0020
+            0x98C7C119,  # stb   r6, 0xC119(r7)
+
+            0x4E800020,  # blr
+        ]
+        dol_data.seek(addr_code)
+        for instr in custom_method:
+            dol_data.write(struct.pack(">I", instr))
+            
+    def t0_memroy_to_dol_address(self, mem_address):
+        return 0x100 + (mem_address - 0x80003100)
+
+    def t1_memroy_to_dol_address(self, mem_address):
+        return 0x25E0 + (mem_address - 0x800055e0)
 
     def _check_versions(self, output_data):
         """
